78cc766a6522d342331fe76fcf5f71a443d40500
SumaGayathri-999 <sumagayathrikonala9393@gmail.com>
2023-03-18 11:40:40 +0530

    new change

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const request = axios.create(
    { baseURL : "https://youtube.googleapis.com/youtube/v3",
     params:{
-        key:process.env.REACT_APP_API_KEY
+        key:"AIzaSyA3zCZw4vj5b-EebbQNaW1gX66WUMgkUM4"
     }}
 )
 export default request;
