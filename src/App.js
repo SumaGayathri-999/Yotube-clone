07b09f8491731e1ef664baecbc4eec7646573dda
Suma Gayathri <104271789+SumaGayathri-999@users.noreply.github.com>
2023-12-31 13:04:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,110 @@
 import React,{useState,useEffect} from 'react'
+import {lazy} from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
-import Header from './Components/header/Header'
-import SideBar from './Components/sideBar/SideBar';
-import HomeScreen from './Components/screens/home/HomeScreen';
 import "./_app.scss";
-import LoginScreen from './Components/screens/login/LoginScreen';
 import {Routes,Route } from 'react-router-dom';
-import Watchscreen from './Components/screens/watchScreen/Watchscreen';
-import SearchScreen from './Components/screens/SearchScreen';
+import LoginScreen from './Components/screens/login/LoginScreen';
+const Header = lazy(()=>import('./Components/header/Header'));
+const SideBar  = lazy(()=>import ('./Components/sideBar/SideBar'));
+const HomeScreen = lazy(()=>import('./Components/screens/home/HomeScreen'));
+const Watchscreen = lazy(()=>import('./Components/screens/watchScreen/Watchscreen'));
+const SearchScreen = lazy(()=>import('./Components/screens/SearchScreen'));
 
 
-const Layout = ({children})=>{
-  const [isSidebar,setSidebar] = useState(false);
 
-  const toggleSidebar = ()=>{
-    setSidebar(isSidebar => !isSidebar)
-  }
+
+const Layout = ({children}) => {
+  const [isSidebar, setSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebar((prevSidebar) => !prevSidebar);
+  };
+
   return (
     <>
-       <Header toggleSidebar={toggleSidebar}/>
-       <div className="app__container ">
-          <SideBar  isSidebar = {isSidebar} toggleSidebar={toggleSidebar} />
-        <Container  fluid className="app__main" >
-              {children}
-          </Container>
-
-       </div> 
+      <Header toggleSidebar={toggleSidebar} />
+      <div className="app__container ">
+        <SideBar isSidebar={isSidebar} toggleSidebar={toggleSidebar} />
+        <Container fluid className="app__main">
+          {children}
+        </Container>
+      </div>
     </>
-  )
-}
+  );
+};
 
 function App() {
-  const {accesstoken,loading} = useSelector(state => state.auth);
+  const {accesstoken, loading} = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  useEffect(()=>{
-     if((!accesstoken) && (!loading)){
-       navigate("/")
-     }
-  },[accesstoken,loading]);
+
+  useEffect(() => {
+    if (!accesstoken && !loading) {
+      navigate("/");
+    }
+  }, [accesstoken, loading]);
+
   return (
     <>
-         <Routes>
-            <Route path="/" element={<LoginScreen />}></Route>
-            <Route path="/home" element={<Layout><HomeScreen /></Layout>}></Route>
-            <Route path='/watch/:id' element={<Layout><Watchscreen /></Layout>}></Route>
-            <Route path='/search/:query' element={<Layout><SearchScreen /></Layout>}></Route>
-            <Route path="*" element={<h1 style={{display:"flex",justifyContent:"center",alignContent:"center",marginTop:"50vh"}}>404 Not Found 😞</h1>} ></Route>
-         </Routes>
+      <Routes>
+        <Route path="/" element={<LoginScreen />} />
+        <Route path="/home" element={
+            <Layout>
+              <React.Suspense fallback={<div style={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                marginTop: "50vh",
+              }}>Loading...</div>}>
+                  <HomeScreen />
+                </React.Suspense>
+            </Layout>
+          }
+        />
+        <Route path="/watch/:id" element={
+            <Layout>
+              <React.Suspense fallback={<div style={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                marginTop: "50vh",
+              }}>Loading...</div>}>
+                  <Watchscreen />
+               </React.Suspense>
+            </Layout>
+          }
+        />
+        <Route path="/search/:query" element={
+            <Layout>
+              <React.Suspense fallback={<div style={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                marginTop: "50vh",
+              }}>Loading...</div>}>
+                  <SearchScreen />
+               </React.Suspense>
+            </Layout>
+          }
+        />
+        <Route path="*" element={
+            <h1
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignContent: "center",
+                marginTop: "50vh",
+              }}
+            >
+              404 Not Found 😞
+            </h1>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
